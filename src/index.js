9d952146a65921a6dae171f387824f9830e8bb1b
Shamil <badalloffsh@gmail.com>
2021-04-18 18:40:04 +0200

    Add: firebase context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { firebase } from "./lib/firebase";
+import { firebase, FieldValue } from "./lib/firebase";
+import FirebaseContext from "./context/firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
