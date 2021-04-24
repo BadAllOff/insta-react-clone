a5459463d7382b9e52f9732ea767c7dd860e065e
Shamil <badalloffsh@gmail.com>
2021-04-24 17:10:11 +0200

    Add: handlelogin

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Getting Started with Create React App
 
 - TailwindCSS
+
+
+WARNING! to run this app you should create your own firebase.js file in lib folder with your own credentials
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
 import * as ROUTES from "../constants/routes";
 
 export default function Login() {
+  const { firebase } = useContext(FirebaseContext);
+
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
   const isInvalid = password === "" || emailAddress === "";
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+    } catch (error) {
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = "Login - Instagram";
@@ -28,14 +45,14 @@ export default function Login() {
               className="mt-2 w-6/12 mb-4"
             />
           </h1>
+          {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
 
-          <form method="POST">
+          <form onSubmit={handleLogin} method="POST">
             <input
               aria-label="Enter your email address"
               className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
               type="text"
               placeholder="Email address"
-              value={emailAddress}
               onChange={({ target }) => setEmailAddress(target.value)}
             />
             <input
@@ -43,7 +60,6 @@ export default function Login() {
               className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
               type="password"
               placeholder="Password"
-              value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
             <button
