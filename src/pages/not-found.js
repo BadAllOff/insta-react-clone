2eed276c0ebb26172eae865f86fab3c4b6f15489
Shamil <badalloffsh@gmail.com>
2021-04-27 15:48:50 +0200

    Add: not-found fallback option page

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 export default function NotFound() {
-    return (
-        <p>Hello from 404</p>
-    )
+  useEffect(() => {
+    document.title = "404 - Not Found";
+  });
+
+  return (
+    <div className="h-screen bg-gray-500">
+      <div className="h-full mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found!</p>
+      </div>
+    </div>
+  );
 }
