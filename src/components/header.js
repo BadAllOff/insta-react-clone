6552a5ff50ffb2fa2ec03fe4ed596fcd3e4d146d
Shamil <badalloffsh@gmail.com>
2021-04-28 18:35:31 +0200

    Add: login btn and avatars

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
 import * as ROUTES from "../constants/routes";
 
 export default function Header() {
+  const { firebase } = useContext(FirebaseContext);
+  // const user = {displayName: 'dali'};
+  const user = null;
+
   return (
     <header className="h-16 bg-white border-b mb-8">
       <div className="container mx-auto max-width-lg h-full">
@@ -17,6 +22,56 @@ export default function Header() {
                 />
               </Link>
             </h1>
+          </div>
+          <div className="text-gray text-center flex items-center align-items">
+            {user ? (
+              <>
+                <Link to={ROUTES.DASHBOARD} arial-label="Home">
+                  <p>Dashboard</p>
+                </Link>
+
+                <button
+                  type="button"
+                  title="SIgn Out"
+                  onClick={() => firebase.auth().signOut()}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      firebase.auth().signOut();
+                    }
+                  }}
+                >
+                  Sign Out
+                </button>
+                <div className="flex items-center cursor-pointer">
+                  <Link to={`/p/${user.displayName}`}>
+                    <img
+                      className="rounded-full h-8 w-8 flex"
+                      src={`/images/avatars/${user.displayName}.jpg`}
+                      alt={`${user.displayName} profile picture`}
+                    />
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <>
+                <Link to={ROUTES.LOGIN}>
+                  <button
+                    type="button"
+                    className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8"
+                  >
+                    Log In
+                  </button>
+                </Link>
+                <Link to={ROUTES.SIGN_UP}>
+                  <button
+                    type="button"
+                    className="font-bold text-sm rounded text-blue w-20 h-8"
+                  >
+                    Sign Up
+                  </button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
