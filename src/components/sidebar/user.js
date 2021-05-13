0c41f5d5b2cdf298fdb11304faf4911628fae903
Shamil <badalloffsh@gmail.com>
2021-05-13 16:39:19 +0200

    Add: profile image and link into sidebar

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import { Link } from "react-router-dom";
 import Skelton from "react-loading-skeleton";
 
@@ -8,10 +8,20 @@ const User = ({ username, fullName }) =>
   ) : (
     <Link
       to={`/p/${username}`}
-      className="grid -grid-cols-4 gap-4 mb-4 items-center"
+      className="grid grid-cols-4 gap-4 mb-4 items-center"
     >
-      <p>{username}</p>
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt="My profile"
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
     </Link>
   );
 
-export default User;
+export default memo(User);
