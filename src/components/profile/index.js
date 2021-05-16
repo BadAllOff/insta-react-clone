00e57ed54b5dc68ed2aa8b7b85ebbdfdfc391220
Shamil <badalloffsh@gmail.com>
2021-05-16 11:21:52 +0200

    Add: user profile page and profile header

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
 import Header from "./header";
 import Photos from "./photos";
+import {
+  getUserByUsername,
+  getUserPhotosByUsername,
+} from "../../services/firebase";
+
+const reducer = (state, newState) => ({ ...state, ...newState });
+const initialState = {
+  profile: {},
+  photosCollection: null,
+  followerCount: 0,
+};
 
 export default function Profile({ username }) {
+  const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(() => {
+    // todo: caching here in localstorage so we don't make a network call we have already made previously
+    async function getProfileInfoAndPhotos() {
+      const [{ ...user }] = await getUserByUsername(username);
+      const photos = await getUserPhotosByUsername(username);
+
+      dispatch({
+        profile: user,
+        photosCollection: photos,
+        followerCount: user.followers.length,
+      });
+    }
+    getProfileInfoAndPhotos();
+  }, [username]);
+
   return (
     <>
-      <Header />
-      <Photos />
+      <Header
+        photosCount={photosCollection ? photosCollection.length : 0}
+        profile={profile}
+        followerCount={followerCount}
+        setFollowerCount={dispatch}
+        username={username}
+      />
+      <Photos photos={photosCollection} />
     </>
   );
 }
